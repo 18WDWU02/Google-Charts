bb89f72b8bffe9b907613c7af92dfcafceb03ec4
18wdwu07 <40220616+18wdwu07@users.noreply.github.com>
2018-08-01 12:32:16 +1200

    added Gauge Chart

---
+++ GaugeChart/js/script.js
@@ -0,0 +1,36 @@
+google.charts.load('current', {'packages':['gauge']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['PhysicalQuantity', 'Value'],
+          ['Temperature', 37],
+          ['Pressure', 105]
+          
+        ]);
+
+        var options = {
+          width: 500, height: 500,
+          redFrom: 30, redTo: 35,
+          yellowFrom:35, yellowTo: 40,
+          minorTicks: 5
+        };
+
+        var chart = new google.visualization.Gauge(document.getElementById('chartContainer'));
+
+        chart.draw(data, options);
+
+        setInterval(function() {
+          data.setValue(0, 1, 30 + Math.round(60 * Math.random()));
+          chart.draw(data, options);
+        }, 10000);
+        setInterval(function() {
+          data.setValue(1, 1, 90 + Math.round(60 * Math.random()));
+          chart.draw(data, options);
+        }, 5000);
+        
+      }
+
+
+//End of page
